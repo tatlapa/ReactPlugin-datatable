3ea3e0994dd3dcda82ae221759ddd178f5e28831
Richard Jérémy <jeremy.rc77@gmail.com>
2024-09-19 12:19:20 +0700

    rowtable fix + add comments for typeDoc

--- src/components/RowTable.tsx
+++ src/components/RowTable.tsx
@@ -5,18 +5,22 @@ interface RowTableProps {
   rows: Array<{ [key: string]: string | number | Date }>;
 }
 
+/**
+ * RowTable component to render table cells for each row.
+ * 
+ * @param {RowTableProps} props - The properties for the RowTable component.
+ * @returns {JSX.Element} The rendered table cells.
+ */
 const RowTable: React.FC<RowTableProps> = ({ rows }) => {
   return (
     <>
-      {rows.map((row, index) => (
-        <tr key={index}>
-          {Object.entries(row).map(([key, value]) => (
-            <td key={key} className="dt-px-4 dt-py-2">
-              {value instanceof Date ? value.toLocaleDateString() : value}
-            </td>
-          ))}
-        </tr>
-      ))}
+      {rows.map((row, index) =>
+        Object.entries(row).map(([key, value]) => (
+          <td key={`${index}-${key}`} className="dt-px-4 dt-py-2">
+            {value instanceof Date ? value.toLocaleDateString() : value}
+          </td>
+        ))
+      )}
     </>
   );
 };
